7b6996cdd874a18d3789122137e369a204ee22bc
Shaswin <shaswinayyan@gmail.com>
2023-06-12 23:51:46 +0530

    Made changes in the final_project directory but still not working

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,14 @@
+import unittest
+from translator import englishToFrench, frenchToEnglish
+
+def test_english_to_french_translation(self):
+    self.assertEqual(english_to_french("hello"), "bonjour")
+    self.assertEqual(english_to_french("world"), "monde")
+self.assertNotEqual(english_to_french('hello'), 'hello')
+
+def test_french_to_english_translation(self):
+    self.assertEqual(french_to_english("bonjour"), "hello")
+    self.assertEqual(french_to_english("monde"), "world")
+self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
+if __name__ == '__main__':
+    unittest.main()
