e7b7f11a5bc5f075f9544d1a2be77e18e8d8af05
Shaswin <shaswinayyan@gmail.com>
2023-06-15 09:52:22 +0530

    made changes in server.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,14 +1,26 @@
 import unittest
+
+from deep_translator import MyMemoryTranslator
 from translator import englishToFrench, frenchToEnglish
 
-def test_english_to_french_translation(self):
-    self.assertEqual(english_to_french("hello"), "bonjour")
-    self.assertEqual(english_to_french("world"), "monde")
-self.assertNotEqual(english_to_french('hello'), 'hello')
+class TestE2F(unittest.TestCase):
+    def test1(self):
+        """English to French test"""
+    def test2(self):
+        # Test that Hello returns Bonjour
+        self.assertEqual(englishToFrench('hello'), 'bonjour')
+        # Test that Hello does not return Hello
+        self.assertNotEqual(englishToFrench('hello'),'hello')
+class TestF2E(unittest.TestCase):
 
-def test_french_to_english_translation(self):
-    self.assertEqual(french_to_english("bonjour"), "hello")
-    self.assertEqual(french_to_english("monde"), "world")
-self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
+    def test1(self):
+        """French to English tests"""
+
+    def test2(self):
+        # Test Bonjour returns Hello
+        self.assertEqual(frenchToEnglish('bonjour'),'hello')
+        # Test Bonjour does not return Bonjour
+        self.assertNotEqual(frenchToEnglish('bonjour'),'bonjour')
+            
 if __name__ == '__main__':
     unittest.main()
